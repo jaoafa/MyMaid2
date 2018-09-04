348d0d779bbfae11b55fbc0131b85e8d6cff6115
Tomachi[Main] <lego@aol.jp>
2018-09-04 17:25:58 +0900

    remove: DeathToDeath disabled

--- src/main/java/com/jaoafa/MyMaid2/Event/Event_DeathToDeath.java
+++ src/main/java/com/jaoafa/MyMaid2/Event/Event_DeathToDeath.java
@@ -1,6 +1,5 @@
 package com.jaoafa.MyMaid2.Event;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -16,6 +15,8 @@ public class Event_DeathToDeath extends MyMaid2Premise implements Listener {
 	}
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onAsyncPlayerChatEvent(AsyncPlayerChatEvent event){
+		return;
+		/*
 		String message = event.getMessage();
 		if(!message.contains("死")){
 			return;
@@ -23,5 +24,6 @@ public class Event_DeathToDeath extends MyMaid2Premise implements Listener {
 		Player player = event.getPlayer();
 		player.chat("私は喋ってはならない言葉を話してしまった…");
 		player.setHealth(0.0D);
+		*/
 	}
 }
