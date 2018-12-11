51cfa57e33b658803cffb9c641be620f10c316b6
Nonsence <hikahikaru17@gmail.com>
2018-12-11 21:25:59 +0900

    Update Cmd_Chat.java

--- src/main/java/com/jaoafa/MyMaid2/Command/Cmd_Chat.java
+++ src/main/java/com/jaoafa/MyMaid2/Command/Cmd_Chat.java
@@ -9,6 +9,8 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import com.jaoafa.MyMaid2.MyMaid2Premise;
+
 public class Cmd_Chat implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
         if(args.length >= 1 && args[0].equalsIgnoreCase("help")){
