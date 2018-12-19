d320f6c07eaf47a3194a1b0491e068aea6fe9da5
Nonsence <hikahikaru17@gmail.com>
2018-12-19 17:12:53 +0900

    Update Team1000Observer.java
    typo (エンティティは人とは限らない)

--- src/main/java/com/jaoafa/MyMaid2/Task/Team1000Observer.java
+++ src/main/java/com/jaoafa/MyMaid2/Task/Team1000Observer.java
@@ -8,8 +8,8 @@ public class Team1000Observer extends BukkitRunnable {
 	@Override
 	public void run() {
 		for (Team team : Bukkit.getServer().getScoreboardManager().getMainScoreboard().getTeams()) {
-			if (team.getSize() >= 1000) { // そのチームに1000人以上いるなら
-				Bukkit.broadcastMessage("[TeamObserver] チーム「" + team.getName() + "」に1000人以上のエンティティが含まれていたため、削除しました。(" + team.getSize() + " エンティティ)");
+			if (team.getSize() >= 1000) { // そのチームに1000体以上いるなら
+				Bukkit.broadcastMessage("[TeamObserver] チーム「" + team.getName() + "」に1000体以上のエンティティが含まれていたため、削除しました。(" + team.getSize() + " エンティティ)");
 				team.unregister();
 			}
 		}
