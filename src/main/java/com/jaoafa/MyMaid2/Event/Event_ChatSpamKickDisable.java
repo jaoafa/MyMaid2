7d74f94f8cc11ddc011d0b8ef40a1e9c37e38ce8
Tomachi[Main] <lego@aol.jp>
2019-01-27 08:29:29 +0900

    change: キックメッセージを通知するように

--- src/main/java/com/jaoafa/MyMaid2/Event/Event_ChatSpamKickDisable.java
+++ src/main/java/com/jaoafa/MyMaid2/Event/Event_ChatSpamKickDisable.java
@@ -1,11 +1,15 @@
 package com.jaoafa.MyMaid2.Event;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerKickEvent;
 
 import com.jaoafa.MyMaid2.MyMaid2Premise;
+import com.jaoafa.MyMaid2.Lib.PermissionsManager;
 
 public class Event_ChatSpamKickDisable extends MyMaid2Premise implements Listener {
 	@EventHandler(priority = EventPriority.HIGHEST)
@@ -13,6 +17,15 @@ public class Event_ChatSpamKickDisable extends MyMaid2Premise implements Listene
 		if(event.getReason().equals("Kicked for spamming")){
 			event.setCancelled(true);
 			return;
+		}
+		Player player = event.getPlayer();
+		String reason = event.getLeaveMessage();
+		for(Player p : Bukkit.getOnlinePlayers()){
+			String group = PermissionsManager.getPermissionMainGroup(p);
+			if(!group.equalsIgnoreCase("Admin") && !group.equalsIgnoreCase("Moderator") && !group.equalsIgnoreCase("Regular")){
+				continue;
+			}
+			p.sendMessage("[MyMaid] " + ChatColor.RED + "プレイヤー「" + player.getName() + "」は、「" + reason + "」という理由でキックされました。");
 		}
 	}
 }
